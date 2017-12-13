6eb951806b6ea79b0f45b8e7ca8ca4b0b8fdac18
EgidijusMaciulis <maciulis.egidijus@gmail.com>
2017-12-13 19:42:57 +0200

    prideta

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pirmasis php</title>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Francois+One&amp;subset=latin-ext" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+
+<?php
+
+//$a =2;
+//$b =$a * 3;
+//$c =$a / 4;
+//$d =$a % 4;//% - likutis
+//$a *=2;// tas pats kas $a = $a * 2
+//
+//$a = 8;
+//$a++;
+//++$a;
+//$x = $a-- +2; // tuo veiksmo metu pasimazino, 8 + 2
+//$y = ++$a * 2; //  7 + 2
+//
+//
+//
+//$pareigos = "Marshal";
+//$pavarde = 'D\'Artagnan';
+//
+//$pasisveikinimas = "Hello ".$pareigos." ".$pavarde;
+//
+//echo $pasisveikinimas;
+
+
+$a = 'D\'Artagnan';
+$x = count($a);
+$y = htmlentities("<b>{$a}</b>");
+$z = html_entity_decode($y);
+
+echo $a."<br>";
+echo $x."<br>";
+echo $y."<br>";
+echo $z."<br>";
+
+$favcolor = "red";
+
+switch($favcolor) {
+    case "red";
+        echo "Your favorite color is red!";
+        break;
+    case "blue";
+        echo "Your favorite color is blue!";
+        break;
+    case "green";
+        echo "Your favorite color is green!";
+        break;
+    default;
+        echo "Your favorite color is neither red, blue, nor green!";
+}
+
+?>
+
+
+
+
+    </body>
+</html>
