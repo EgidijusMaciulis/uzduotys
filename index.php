6b49af20b647061b143f4b9220e5e1b7c20e167f
EgidijusMaciulis <maciulis.egidijus@gmail.com>
2017-12-18 13:01:07 +0200

    Namu darbas 2

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Puslapio Antraste</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+    
+    <body>
+    <table>
+            <tr>
+                <td colspan="2"  id="head">Pavadinimas</td>
+                <td  id="head">Veiksmas</td>
+            </tr>
+        <tr>
+            <td colspan="3" class="food">Vaisiai</td>
+        </tr>
+        <tr>
+            <td>Ananasai</td>
+            <td><img src="https://www.capitalfm.co.ke/lifestyle/files/2013/11/Pineapple.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Granatai</td>
+            <td><img src="https://4.imimg.com/data4/HP/FK/MY-33563211/fresh-pomegranate-500x500.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Kriauses</td>
+            <td><img src="https://www.ekomarket.lt/images/uploader/kr/kriauses-conference-1.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Obuoliai</td>
+            <td><img src="https://www.iki.lt/assets/Uploads/Obuoliai-Pepinai.png"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Bananai</td>
+            <td><img src="https://5.imimg.com/data5/KP/PR/MY-12396329/ripened-banana-500x500.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Apelsinai</td>
+            <td><img src="https://sc02.alicdn.com/kf/HTB1quYhKVXXXXb6XXXXq6xXFXXX2/natural-orange-flavour.jpg_350x350.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td colspan="3" class="food">Daržovės</td>
+        </tr>
+        <tr>
+            <td>Pomidorai</td>
+            <td><img src="http://assets.nydailynews.com/polopoly_fs/1.2834910.1476795531!/img/httpImage/image.jpg_gen/derivatives/article_750/488059328.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Paprikos</td>
+            <td><img src="https://cdn.shopify.com/s/files/1/1380/2059/products/Capsicum-Red_600x600.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Svogunai</td>
+            <td><img src="https://pbs.twimg.com/media/CS40eW4WsAAqqN_.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Agurkai</td>
+            <td><img src="http://9975-presscdn-0-95.pagely.netdna-cdn.com/wp-content/uploads/2016/05/cucumber01-lg.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Brokoliai</td>
+            <td><img src="https://www.iki.lt/assets/Uploads/_resampled/ResizedImageWzIwMCwyMDBd/brokolis.png"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+        <tr>
+            <td>Kopustai</td>
+            <td><img src="https://images-na.ssl-images-amazon.com/images/I/41nZ3l1hWvL._SX355_.jpg"></td>
+            <td><button>Pirkti</button></td>
+        </tr>
+
+    </table>
+    </body>
+
+</html>
