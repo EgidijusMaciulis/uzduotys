850cb88b94660eb042ce5e812610b6ea2c6e7e1a
Egidijus <31761153+EgidijusMaciulis@users.noreply.github.com>
2017-12-19 17:55:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,42 +10,74 @@
 <?php
 
 
-$a =
-    [
-        'Jonas',
-        'Petras',
-        'Antanas',
-        'Povilas'
-    ];
+$a = [
+    'Jonas',
+    'Petras',
+    'Antanas',
+    'Povilas'];
 
-$pairs = [];
-
-foreach($a as $k => $v) {
-    foreach(array_slice($a, $k + 1) as $k2 => $v2) {
-        $pairs[] = [$v, $v2];
+for ($i = 0; $i < count($a); $i++) {
+    for ($j = $i + 1; $j < count($a); $j++) {
+        echo $a[$i] . "-" . $a[$j] . "<br>";
     }
 }
-var_dump($pairs);
+echo "<br>";
+
+for ($i = 0; $i < count($a); $i++) {
+    for ($j = 0; $j < count($a); $j++) {
+        if ($i == $j) {
+            continue;
+        } else {
+            echo $a[$i] . "-" . $a[$j] . "<br>";
+        }
+    }
+}
+
+echo "<br>";
 
 
-
-$array = [
+$b = [
     [1, 3, 4],
     [2, 5],
     [2 => 3, 5 => 8],
     [1, 1, 5 => 1]
 ];
 
-$sum = $array;
+$suma = 0;
+$maxSuma = [];
+$k = 1;
+for ($j = 0; $j <= $k; $j++) {
 
-foreach($array[0] as $m => $n)
-    $sum[$m] = array_sum(array_column($array, $m));
+    for ($i = 0; $i < count($b); $i++) {
+        if (isset($b[$i][$j])) {
+            $suma += $b[$i][$j];
 
-echo "Stulpeliu sumos <br>";
+        }
 
+        end($b[$i]);
+        $k = key($b[$i]);
 
-print_r($sum);
+    }
+    array_push($maxSuma, $suma);
 
+    $suma = 0;
+}
+print_r($maxSuma);
+function didziausias()
+{
+    $max = 0;
+    global $maxSuma;
+    foreach ($maxSuma as $value) {
+        if ($value > $max) {
+            $max = $value;
+        }
+    }
+    echo $max;
+
+}
+
+echo "<br> Didziausia stulpelio suma: ";
+didziausias();
 
 
 
